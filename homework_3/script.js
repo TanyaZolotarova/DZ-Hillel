26e6905c923482438649bf0f95bff76b7aeba10a
TanyaZolotarova <51660611+TanyaZolotarova@users.noreply.github.com>
2024-06-18 15:16:06 +0300

    output data types using typeof

---
+++ homework_3/script.js
@@ -0,0 +1,19 @@
+//output data types using typeof
+
+const str = 'Hello World!';
+const num = 55;
+const bigIntValue = 55n;
+const emptyValue = null;
+const undefinedValue = undefined;
+const isTrue = true;
+const symbol = Symbol("id");
+const obj = {a:1, b:2};
+
+console.log('str ==',typeof str);
+console.log('num ==',typeof num);
+console.log('bigIntValue ==',typeof bigIntValue);
+console.log('emptyValue ==',typeof emptyValue);
+console.log('undefinedValue ==',typeof undefinedValue);
+console.log('isTrue ==',typeof isTrue);
+console.log('symbol ==',typeof symbol);
+console.log('obj ==',typeof obj);
