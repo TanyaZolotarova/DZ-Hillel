d8c6bc8b3223c62f0c0e8dd93c46266e2d011099
TanyaZolotarova <51660611+TanyaZolotarova@users.noreply.github.com>
2024-06-20 22:32:05 +0300

    created a modal window with a question for the user

---
+++ homework_4/script.js
@@ -0,0 +1,5 @@
+//Use the prompt to ask for the "username"
+//Use alert to print "Hello, John! How are you?", where "John" is what the user entered
+
+const username = prompt('Please enter your name:');
+alert(`Hello, ${username}! How are you?`);
