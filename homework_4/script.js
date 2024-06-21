384270ace75e64926fc22edaf57e2869b4ac4572
TanyaZolotarova <51660611+TanyaZolotarova@users.noreply.github.com>
2024-06-21 14:18:58 +0300

    rewrite the code below using switch

--- homework_4/script.js
+++ homework_4/script.js
@@ -3,3 +3,34 @@
 
 const username = prompt('Please enter your name:');
 alert(`Hello, ${username}! How are you?`);
+
+
+/* Rewrite the code below using the construct switch…case:
+let numOrStr = prompt('input number or string');
+console.log(numOrStr)
+if(numOrStr === null) {
+    console.log('ви скасували')
+} else if( numOrStr.trim() === '' ) {
+    console.log('Empty String');
+} else if ( isNaN( +numOrStr ) ) {
+    console.log(' number is Ba_NaN')
+} else {
+    console.log('OK!')
+} */
+
+const numOrStr = prompt('input number or string');
+
+switch (true){
+    case numOrStr === null:
+        console.log('ви скасували')
+        break;
+    case numOrStr.trim() === '':
+        console.log('Empty String');
+        break;
+    case isNaN( +numOrStr ):
+        console.log(' number is Ba_NaN');
+        break;
+    default:
+        console.log('OK!');
+        break;
+}
