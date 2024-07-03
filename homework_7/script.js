b736ba08451fe64cfcd9c9bcfc7bd7ebdc08fd41
TanyaZolotarova <51660611+TanyaZolotarova@users.noreply.github.com>
2024-07-03 23:54:30 +0300

    create closure function

--- homework_7/script.js
+++ homework_7/script.js
@@ -15,3 +15,42 @@ console.log('Currying sumOfNum arrow function ---------- == ', sumOfNum);
 
  const name = CarryNumbersSum(5)(2);
 console.log('Currying name function Declaration ---------- == ', name);
+
+
+
+// Написати функцію, яка приймає 1 параметр. Та скадае значення з тим, що передали перший раз і т. д. Все це із замиканнями, наприклад:
+// console.log(sum(4)); // 4
+// console.log(sum(6)); // 10
+// console.log(sum(10)); // 20
+// console.log(sum(7)); // 27
+
+
+function createIncrementor() {
+    let n = 0;
+    return function (num){
+        return n += num
+    }
+}
+
+const sum = createIncrementor();
+
+console.log(sum(4));
+console.log(sum(6));
+console.log(sum(10));
+console.log(sum(7));
+
+
+function createSum(n) {
+    return function (num){
+        n += num
+        return n;
+    }
+}
+
+const sumNum = createSum(0);
+
+console.log(sumNum(4));
+console.log(sumNum(6));
+console.log(sumNum(10));
+console.log(sumNum(7));
+
