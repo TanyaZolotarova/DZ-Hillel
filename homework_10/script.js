c13e4dd5071ad09210f6f5d40036d24e44a93983
TanyaZolotarova <51660611+TanyaZolotarova@users.noreply.github.com>
2024-07-18 14:26:43 +0300

    create a contact book object with find and add methods

--- homework_10/script.js
+++ homework_10/script.js
@@ -35,3 +35,43 @@ const arrNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 const newArrNumbers = arrNumbers.filter(num => num % 2 === 0);
 
 console.log('newArrNumbers ----------- == ', newArrNumbers)
+
+
+
+/*ДЗ 10.3. Книга контактів
+Створіть об'єкт, який матиме одну властивість з масивом об'єктів. Які представляють контакти у вашій контактній книзі.
+Кожен об'єкт має містити ім'я, номер телефону та адресу електронної пошти.
+ Додайте метод для пошуку контакту за ім'ям та метод для додавання нових контактів.
+*/
+
+
+const bookContacts = {
+    contactsInfo: [
+        {name: 'John',
+        phone: 8057123456,
+        email: 'johntest@gmail.com'},
+        {name: 'Sansa',
+            phone: 8057123457,
+            email: 'sansatest@gmail.com'},
+        {name: 'Arya',
+            phone: 8057123458,
+            email: 'aryatest@gmail.com'}
+    ]
+};
+
+
+function addContact (name, phone, email){
+    const newContact = {name, phone, email};
+    bookContacts.contactsInfo.push(newContact);
+}
+
+function findContactByName(name) {
+    return bookContacts.contactsInfo.filter(contact => contact.name.toLowerCase() === name.toLowerCase());
+}
+
+addContact('Bran', 8057123459, 'brantest@gmail.com');
+
+const contact = findContactByName('Bran');
+console.log('Find contact --------------- == ',contact);
+
+console.log('All book contact -------------- == ', bookContacts);
